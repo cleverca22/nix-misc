2fe0715e783fd591e11d4459db77bdbdf309494b
michael bishop <cleverca22@gmail.com>
2016-03-05 12:55:52 -0400

    add qemu-user

---
+++ qemu-user.nix
@@ -0,0 +1,39 @@
+{ stdenv, fetchurl, python, pkgconfig, zlib, glib, user_arch, flex, bison, makeStaticLibraries }:
+
+let
+  env2 = makeStaticLibraries stdenv;
+  myzlib = zlib.overrideDerivation (args: {
+    configureFlags = toString args.configureFlags or "" + " --static";
+  });
+  myglib = glib.override { stdenv = env2; };
+in
+stdenv.mkDerivation rec {
+  name = "qemu-user-${user_arch}-${version}";
+  version = "2.4.0";
+  buildInputs = [ python pkgconfig myzlib myglib flex bison ];
+  src = fetchurl {
+    url = "http://wiki.qemu.org/download/qemu-${version}.tar.bz2";
+    sha256 = "0836gqv5zcl0xswwjcns3mlkn18lyz2fiq8rl1ihcm6cpf8vkc3j";
+  };
+  configureFlags = [
+    "--enable-linux-user" "--target-list=${user_arch}-linux-user"
+    "--disable-bsd-user" "--disable-system" "--disable-vnc" "--without-pixman"
+    "--disable-vnc-tls" "--disable-curses" "--disable-sdl" "--disable-vde"
+    "--disable-bluez" "--disable-kvm"
+    "--static"
+    "--disable-tools"
+  ];
+  postInstall = ''
+    cat <<EOF > $out/bin/register
+    #!/bin/sh
+    modprobe binfmt_misc
+    mount -t binfmt_misc binfmt_misc  /proc/sys/fs/binfmt_misc
+    ${
+      if user_arch == "arm" then ''echo   ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\x00\xff\xfe\xff\xff\xff:$out/bin/qemu-arm:' > /proc/sys/fs/binfmt_misc/register''
+      else if user_arch == "aarch64" then ''echo   ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\x00\xff\xfe\xff\xff\xff:$out/bin/qemu-aarch64:' > /proc/sys/fs/binfmt_misc/register''
+      else "echo unknown arch"
+    }
+    EOF
+    chmod +x $out/bin/register
+  '';
+}
--- root.nix
+++ root.nix
@@ -1,4 +1,6 @@
 with import <nixpkgs> {};
 {
   toxvpn = callPackage ./toxvpn.nix {};
+  qemu-user-arm = pkgs.callPackage ./qemu-user.nix { user_arch = "arm"; };
+  qemu-user-arm64 = pkgs.callPackage ./qemu-user.nix { user_arch = "aarch64"; };
 }
