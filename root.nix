b94cdfbab29c6cc512e54686e477a8ba00021ecf
michael bishop <cleverca22@gmail.com>
2016-03-05 20:26:08 -0400

    add a nix patch

--- root.nix
+++ root.nix
@@ -3,4 +3,7 @@ with import <nixpkgs> {};
   toxvpn = callPackage ./toxvpn.nix {};
   qemu-user-arm = pkgs.callPackage ./qemu-user.nix { user_arch = "arm"; };
   qemu-user-arm64 = pkgs.callPackage ./qemu-user.nix { user_arch = "aarch64"; };
+  nix = pkgs.stdenv.lib.overrideDerivation pkgs.nix (oldAttrs: {
+    patches = ./hax.patch;
+  });
 }
