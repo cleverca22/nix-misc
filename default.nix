7f24e2adc284fdd0f7824f9ca22e188555d5bf32
Michael Bishop <cleverca22@gmail.com>
2018-04-17 02:41:58 -0300

    update to handle nix2

--- default.nix
+++ default.nix
@@ -8,9 +8,6 @@ with pkgs_host;
 {
   toxvpn = callPackage ./toxvpn.nix {};
   nix = pkgs_host.stdenv.lib.overrideDerivation pkgs_host.nix (oldAttrs: {
-    patches = ./upgrade.patch;
-  });
-  nixUnstable = pkgs_host.stdenv.lib.overrideDerivation pkgs_host.nixUnstable (oldAttrs: {
     patches = ./upgrade-unstable.patch;
   });
   nix_arm6 = pkgs_arm6.stdenv.lib.overrideDerivation pkgs_arm6.nix (oldAttrs: {
