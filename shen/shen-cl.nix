d3b10f939c89db914ed74c71829b56fabc5ea581
Michael Bishop <cleverca22@gmail.com>
2018-04-17 11:05:17 -0300

    fix the fixed-output hash

--- shen/shen-cl.nix
+++ shen/shen-cl.nix
@@ -1,18 +1,29 @@
 { stdenv, shen-src, wget, cacert, lib, zlib }:
 
-stdenv.mkDerivation {
-  name = "shen-cl";
-  src = shen-src;
-  outputHashAlgo = "sha256";
-  outputHash = "0bkx2ikg67fg548ss90zmy7516p9k7qkxhyw0n6fb7s5l6nwwk4d";
-  outputHashMode = "recursive";
-  buildInputs = [ wget ];
-  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+let
+  fixedhash = stdenv.mkDerivation {
+    name = "shen-cl";
+    src = shen-src;
+    outputHashAlgo = "sha256";
+    outputHash = "0sgcn33mjkp2ig4mk7wg63kaqbjhh2yzxwk97yk4zk21djd21q2j";
+    outputHashMode = "recursive";
+    buildInputs = [ wget ];
+    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    buildCommand = ''
+      unpackPhase
+      cd $sourceRoot
+      make fetch
+      mv shen-cl $out
+    '';
+  };
+in stdenv.mkDerivation {
+  name = "shen-cl-patched";
+  src = fixedhash;
   buildCommand = ''
     unpackPhase
     cd $sourceRoot
-    make fetch
-    mv shen-cl $out
+    mkdir $out
+    mv * $out/
     patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" --set-rpath ${lib.makeLibraryPath [ zlib ]} $out/shen
   '';
 }
